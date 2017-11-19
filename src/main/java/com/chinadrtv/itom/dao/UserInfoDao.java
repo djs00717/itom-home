fc10882d6a43a8505e8ef7a2902fa14d95db817d
djs <djs00717@126.com>
2017-11-19 16:50:35 +0800

    itom

--- src/main/java/com/chinadrtv/itom/dao/UserInfoDao.java
+++ src/main/java/com/chinadrtv/itom/dao/UserInfoDao.java
@@ -1,10 +1,13 @@
 package com.chinadrtv.itom.dao;
 
+import com.chinadrtv.itom.dto.UserInfoDto;
+import com.chinadrtv.itom.model.UserInfo;
+
 import java.util.List;
 
 /**
  * Created by djs on 2017/11/14.
  */
 public interface UserInfoDao  {
-    public List list();
+    public List<UserInfoDto> list(String loginName);
 }
--- src/main/java/com/chinadrtv/itom/dto/UserInfoDto.java
+++ src/main/java/com/chinadrtv/itom/dto/UserInfoDto.java
@@ -9,8 +9,54 @@ public class UserInfoDto {
     private String userName;
     private String department;
     private Integer isBlocked;
+    private  String email;
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Integer getIsBlocked() {
+        return isBlocked;
+    }
+
+    public void setIsBlocked(Integer isBlocked) {
+        this.isBlocked = isBlocked;
+    }
 
 
+    public String getEmail() {
+        return email;
+    }
 
-
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
