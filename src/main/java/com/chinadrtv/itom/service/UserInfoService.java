64379a2da2320c678965f385fdd114b97f4527d2
djs <djs00717@126.com>
2017-11-19 17:12:49 +0800

    itom

--- src/main/java/com/chinadrtv/itom/service/UserInfoService.java
+++ src/main/java/com/chinadrtv/itom/service/UserInfoService.java
@@ -3,6 +3,7 @@ package com.chinadrtv.itom.service;
 /**
  * Created by djs on 2017/11/8.
  */
+
 public interface UserInfoService {
    public boolean checkUser(String userName,String passWord);
 }
--- src/main/java/com/chinadrtv/itom/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/chinadrtv/itom/service/impl/UserInfoServiceImpl.java
@@ -1,11 +1,13 @@
 package com.chinadrtv.itom.service.impl;
 
 import com.chinadrtv.itom.service.UserInfoService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by djs on 2017/11/8.
  */
-public class UserServiceImpl implements UserInfoService {
+@Service("UserInfoService")
+public class UserInfoServiceImpl implements UserInfoService {
     public boolean checkUser(String userName, String passWord) {
         return false;
     }
