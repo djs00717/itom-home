a56da86ab44e8dcf0bd2bd3c530bdfe233fce6f7
djs <djs00717@126.com>
2017-11-19 17:18:44 +0800

    itom

--- src/main/java/com/chinadrtv/itom/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/chinadrtv/itom/service/impl/UserInfoServiceImpl.java
@@ -1,5 +1,8 @@
 package com.chinadrtv.itom.service.impl;
 
+import com.chinadrtv.itom.dao.UserInfoDao;
+import com.chinadrtv.itom.dao.impl.UserInfoDaoImpl;
+import com.chinadrtv.itom.dto.UserInfoDto;
 import com.chinadrtv.itom.service.UserInfoService;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +11,14 @@ import org.springframework.stereotype.Service;
  */
 @Service("UserInfoService")
 public class UserInfoServiceImpl implements UserInfoService {
-    public boolean checkUser(String userName, String passWord) {
-        return false;
+    public boolean checkUser(String loginName, String passWord) {
+        boolean flag=false;
+        UserInfoDao dao = new UserInfoDaoImpl();
+        UserInfoDto list = dao.list(loginName);
+        if(!list.equals(null)){
+            flag=true;
+        }
+
+        return flag;
     }
 }
